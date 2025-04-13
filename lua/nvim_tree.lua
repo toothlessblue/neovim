6d56735c7cd2fc5c34a26ddcb8935884239df7a2
toothlessblue <ellis.verhoest@gmail.com>
2025-04-13 13:38:27 +0100

    updated tree options

--- lua/nvim_tree.lua
+++ lua/nvim_tree.lua
@@ -7,37 +7,36 @@ vim.opt.termguicolors = true
 
 -- OR setup with some options
 require('nvim-tree').setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = false,
-  },
-  on_attach = function(bufnr)
-    local api = require "nvim-tree.api"
+    sort = {
+        sorter = "case_sensitive",
+    },
+    view = {
+        width = 30,
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = false,
+        git_ignored = false
+    },
+    on_attach = function(bufnr)
+        local api = require "nvim-tree.api"
 
-    local function opts(desc)
-      return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-    end
+        local function opts(desc)
+            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
 
-    api.config.mappings.default_on_attach(bufnr)
-  end,
+        api.config.mappings.default_on_attach(bufnr)
+    end,
 })
 
 local api = require('nvim-tree.api')
 
-vim.keymap.set('n', '<leader>e', function() 
+vim.keymap.set('n', '<leader>e', function()
     if api.tree.is_tree_buf(0) then
         api.tree.close()
     else
         api.tree.open()
     end
 end, {})
-
-
